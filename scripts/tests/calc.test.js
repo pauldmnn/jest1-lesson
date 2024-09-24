c50f456561745c7a1dcca2cc3c795f3a20f8980f
Paul-vasile Dominan <pauldmnn@gmail.com>
2024-09-24 11:22:49 +0000

    Initial commit

---
+++ scripts/tests/calc.test.js
@@ -0,0 +1,22 @@
+const addition = require("../calc");
+
+describe("Calculator", () => {
+    describe("Addition to function", () => {
+        test("Should return 42 for 20 + 22", () => {
+            expect(addition(20, 22)).toBe(42);
+        })
+        test("Should return 73 for 32 + 41", () => {
+            expect(addition(32, 41)).toBe(73);
+        })
+
+    });
+    describe("Subtraction to function", () => {
+        
+    });
+    describe("Multiplication to function", () => {
+        
+    });
+    describe("Division to function", () => {
+        
+    });
+});
